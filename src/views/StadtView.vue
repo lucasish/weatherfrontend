46887e4dd9de00f954d52a61ae2e59f19f5c3cf8
milena <milenakalo@icloud.com>
2022-05-13 13:44:06 +0200

    add dynamic city weather view

--- src/views/StadtView.vue
+++ src/views/StadtView.vue
@@ -1,10 +1,43 @@
 <template>
   <h1>Buh</h1>
+  <div class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col" v-for="city in citys" :key="city.id">
+        <div class="card h-100">
+          <img src="../assets/logo.png" class="card-img-top" :alt="city.name">
+          <div class="card-body">
+            <h5 class="card-title">{{ city.firstName }} {{ city.lastName }}</h5>
+            <p class="card-text">
+              {{ city.name }} {{ city.unit }} ist {{ city.temp }} und
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'StadtView'
+  name: 'StadtView',
+  data () {
+    return {
+      citys: [
+        {
+          id: 1,
+          name: 'Potsdam',
+          unit: 'imperial',
+          temp: 30
+        },
+        {
+          id: 2,
+          name: 'Berlin',
+          unit: 'imperial',
+          temp: 19
+        }
+      ]
+    }
+  }
 }
 </script>
 
