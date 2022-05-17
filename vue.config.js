2c22ee18b381731d6cc8b34757c6789be053254a
Lucas Schneider <44725259+xclm@users.noreply.github.com>
2022-05-17 19:33:48 +0200

    fix3

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   devServer: {
     port: '3000',
-    proxy: 'https://htwweatherapp.herokuapp.com/api' // wg cors fehler hinzugefügt
+    proxy: 'https://htwweatherapp.herokuapp.com/api/v1/weatherofcity' // wg cors fehler hinzugefügt
   }
 }
