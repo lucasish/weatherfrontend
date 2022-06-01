e8e7cf9f5b5948a6d5d139ecb6eafc83dcc16519
milena <milenakalo@icloud.com>
2022-06-01 19:13:19 +0200

    fix homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,44 @@
 <template>
-  <h1>Cities</h1>
+  <h1>Home</h1>
   <div class=".bg-info.bg-gradient bg-opacity-10"></div>
   <div class="container-fluid">
     <div class="row row-cols-1 row-cols-md-4 g-4">
-      <div class="col" v-for="city in citys" :key="city.id">
+      <div class="col" >
         <div class="card h-100">
-            <img :src= "'https://openweathermap.org/img/w/03d.png'"  class="card-img-top" :alt="city.name">
+            <img :src= "`https://openweathermap.org/img/w/${this.citys[1].icon}.png`"  class="card-img-top" :alt="citys[1].name">
             <div id="title" class="card-body">
-              <h5 id="temp" class="card-title"> {{city.name}}</h5>
+              <h5 id="temp" class="card-title"> {{citys[1].name}}</h5>
               <p id="location" class="card-text">
-               In {{ city.name }}:
-                Temperatur {{city.temp}} Grad Celsius.
-                Wetter : {{city.weather.main}}
-                Hinweis: {{city.weather.description}}
+               In {{ citys[1].name }} sind Temperatur: {{citys[1].temp}} Grad Celsius.
+                Wetter : {{citys[1].weather}}
               </p>
             </div>
           </div>
         </div>
+      <div class="col" >
+        <div class="card h-100">
+          <img :src= "`https://openweathermap.org/img/w/${this.citys[0].icon}.png`"  class="card-img-top" :alt="citys[0].name">
+          <div  class="card-body">
+            <h5  class="card-title"> {{citys[0].name}}</h5>
+            <p  class="card-text">
+              In {{ citys[0].name }} sind Temperatur: {{citys[0].temp}} Grad Celsius.
+              Wetter : {{citys[0].weather}}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col" >
+        <div class="card h-100">
+          <img :src= "`https://openweathermap.org/img/w/${this.citys[2].icon}.png`"  class="card-img-top" :alt="citys[2].name">
+          <div  class="card-body">
+            <h5  class="card-title"> {{citys[2].name}}</h5>
+            <p  class="card-text">
+              In {{ citys[2].name }} sind Temperatur: {{citys[2].temp}} Grad Celsius.
+              Wetter : {{citys[2].weather}}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -30,24 +52,21 @@ export default {
       citys: [{
         id: 1,
         name: 'Berlin',
-        temp: 0,
-        main: ' ',
+        temp: ' ',
         weather: ' ',
         icon: ' '
       },
         {
           id: 2,
-          name: 'London',
-          temp: 0,
-          main: ' ',
+          name: 'Miami',
+          temp: ' ',
           weather: ' ',
           icon: ' '
         },
         {
           id: 3,
           name: 'Paris',
-          temp: 0,
-          main: ' ',
+          temp: ' ',
           weather: ' ',
           icon: ' '
         }]
@@ -67,24 +86,70 @@ export default {
       method: 'GET',
       redirect: 'follow'
     }
+
     let obj
-    let i = 0
-    for(i; i < this.citys.length; i++){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=8f72af7c99ac2ff422253044302e41c7&q=${this.citys[i].name}&units=metric`, requestOptions)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=8f72af7c99ac2ff422253044302e41c7&q=${this.citys[1].name}&units=metric`, requestOptions)
       .then(res => {
-        if (res.ok) { return res.json() } else {
+        if (res.ok) {
+          return res.json()
+        } else {
           throw new Error('Error')
         }
       })
       .then(data => {
         obj = data
-        this.citys[i].main = obj.main
-        this.citys[i].temp = obj.main.temp
-        this.citys[i].weather = obj.weather
-        this.citys[i].icon = obj.weather.icon
+        console.log(obj.main.temp)
+        console.log(this.citys[1].name)
+        this.citys[1].temp = obj.main.temp
+        this.citys[1].weather = obj.weather[0].description
+        this.citys[1].icon = obj.weather[0].icon
+        console.log(obj.weather[0].icon)
+        console.log(this.citys[1].icon)
+      })
+      .catch(error => console.log('error', error))
+
+    let obj2
+    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=8f72af7c99ac2ff422253044302e41c7&q=${this.citys[0].name}&units=metric`, requestOptions)
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Error')
+        }
+      })
+      .then(data => {
+        obj2 = data
+        console.log(obj2.main.temp)
+        console.log(this.citys[0].name)
+        this.citys[0].temp = obj2.main.temp
+        this.citys[0].weather = obj2.weather[0].description
+        this.citys[0].icon = obj2.weather[0].icon
+        console.log(obj2.weather[0].icon)
+        console.log(this.citys[0].icon)
+      })
+      .catch(error => console.log('error', error))
+
+    let obj3
+    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=8f72af7c99ac2ff422253044302e41c7&q=${this.citys[2].name}&units=metric`, requestOptions)
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Error')
+        }
+      })
+      .then(data => {
+        obj3 = data
+        console.log(obj3.main.temp)
+        console.log(this.citys[2].name)
+        this.citys[2].temp = obj3.main.temp
+        this.citys[2].weather = obj3.weather[0].description
+        this.citys[2].icon = obj3.weather[0].icon
+        console.log(obj3.weather[0].icon)
+        console.log(this.citys[2].icon)
       })
       .catch(error => console.log('error', error))
 }
-  }
+
 }
 </script>
