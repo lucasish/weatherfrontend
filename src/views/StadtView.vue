33c5718ee422a4f0b700458e5d6fbffa42964d3e
Lucas Schneider <44725259+xclm@users.noreply.github.com>
2022-06-15 00:01:40 +0200

    fix backend connection3?

--- src/views/StadtView.vue
+++ src/views/StadtView.vue
@@ -28,7 +28,8 @@
           </ul>
         </div>
         <div class="col-12">
-          <button id="button2" class="btn btn-primary" type="submit" @click.prevent="createCity()">Stadt hinzufügen</button>
+          <button id="button2" class="btn btn-primary" type="submit" @click="createCity()">Stadt hinzufügen</button>
+<!--          <button v-on:click="createCity()" id="button2" class="btn btn-primary" type="submit">Stadt hinzufügen</button>-->
         </div>
       </form>
     </div>
@@ -84,32 +85,34 @@ export default {
   async createCity () {
     console.log("erkannt",this.name)
     const jsonvalue = JSON.stringify({name: this.name})
-    await axios.post(process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/weatherofcity', jsonvalue, {
+    return await axios.post(process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/weatherofcity', jsonvalue, {
     headers: {
       name: this.name,
       'Content-Type': 'application/json'
       }
     })
-    if (this.validate()) {
-      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/weatherofcity'
-
-      const headers = new Headers()
-      headers.append('Content-Type', 'application/json')
-
-      const city = JSON.stringify({
-        name: this.name
-      })
-
-      const requestOptions = {
-        method: 'POST',
-        headers: headers,
-        body: city,
-        redirect: 'follow'
-      }
-
-      const response = await fetch(endpoint, requestOptions)
-      await this.handleResponse(response)
-    }
+    // const resultat = await axios.post(process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/weatherofcity',
+    // console.log("erhalten")
+    // if (this.validate()) {
+    //   const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/weatherofcity'
+    //
+    //   const headers = new Headers()
+    //   headers.append('Content-Type', 'application/json')
+    //
+    //   const city = JSON.stringify({
+    //     name: this.name
+    //   })
+    //
+    //   const requestOptions = {
+    //     method: 'POST',
+    //     headers: headers,
+    //     body: city,
+    //     redirect: 'follow'
+    //   }
+    //
+    //   const response = await fetch(endpoint, requestOptions)
+    //   await this.handleResponse(response)
+    // }
   }
   },
   async handleResponse (response) {
