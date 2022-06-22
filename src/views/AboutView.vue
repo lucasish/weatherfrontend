603e8212fd0218e3a58eb087d747cedf250db27a
milena <milenakalo@icloud.com>
2022-06-22 17:34:19 +0200

    changes in aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,7 @@
         <div class="card-body">
           <h5 class="card-title">Milena Kalo</h5>
           <p class="card-text">
-            Wirtcshaftsnformatik Bachelorstudent 🎓
+            Wirtschaftsnformatik Bachelorstudent 🎓
           </p>
         </div>
       </div>
