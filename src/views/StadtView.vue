3da8e3ead8d437e6ddfe67a2a8a7fac589dbd8c0
Lucas Schneider <44725259+xclm@users.noreply.github.com>
2022-06-22 15:04:54 +0200

    stand deletebutton

--- src/views/StadtView.vue
+++ src/views/StadtView.vue
@@ -3,6 +3,8 @@
   <h1> Wetter </h1>
   <br>
   <button class="btn btn-primary" id="button" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Neue Stadt hinzufügen</button>
+  <br>
+  <button id="button3" class="btn btn-primary" type="submit" @click.prevent="deleteCities()">Stadt löschen</button>
 
   <div class="offcanvas offcanvas-end" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
     <div class="offcanvas-header">
@@ -133,6 +135,28 @@ export default {
       this.serverValidationMessages.push('Unknown error occurred')
     }
   },
+    async deleteCities () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/weatherofcity'
+
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+      console.log("append ok")
+
+      // const city = JSON.stringify({
+      //   name: this.name
+      // })
+      //   console.log("stringfy ok")
+      //
+      const requestOptions = {
+        method: 'DELETE',
+        headers: headers,
+        // body: city,
+        redirect: 'follow'
+      }
+      const response = await fetch(endpoint, requestOptions)
+      console.log("await fetch ok")
+
+    },
   validate () {
     const form = document.getElementById('city-create-form')
     form.classList.add('was-validated')
